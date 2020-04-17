c9e5c4c5a87552feba6fa1c37f6dfdbf18526431
Guilherme Soares Lima <guilherme.soares@icarros.com.br>
2020-04-17 17:06:54 -0300

    Defindindo estrutura inicial

---
+++ src/components/Main/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="main"></div>;
+  }
+}
+
+export default Main;
